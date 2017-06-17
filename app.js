eb2755daf1bd7c255a285ac806c3449177588bda
sixloop <zhoucx@dtdream.com>
2017-06-17 19:02:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,42 @@
-var http = require('http'),
-    wechat = require('node-wechat');
+var http = require("http");
+var url = require("url");
+var crypto = require("crypto");
 
-wechat.token = 'your_token';
+function sha1(str){
+  var md5sum = crypto.createHash("sha1");
+  md5sum.update(str);
+  str = md5sum.digest("hex");
+  return str;
+}
 
-http.createServer(function (req, res) {
-  //检验 token
-  wechat.checkSignature(req, res);
-  //预处理
-  wechat.handler(req, res);
+function validateToken(req,res){
+  var query = url.parse(req.url,true).query;
+  //console.log("*** URL:" + req.url);
+  //console.log(query);
+  var signature = query.signature;
+  var echostr = query.echostr;
+  var timestamp = query['timestamp'];
+  var nonce = query.nonce;
+  var oriArray = new Array();
+  oriArray[0] = nonce;
+  oriArray[1] = timestamp;
+  oriArray[2] = "zhouchangxun";//这里是你在微信开发者中心页面里填的token，而不是****
+  oriArray.sort();
+  var original = oriArray.join('');
+  console.log("Original str : " + original);
+  console.log("Signature : " + signature );
+  var scyptoString = sha1(original);
+  if(signature == scyptoString){
+    res.end(echostr);
+    console.log("Confirm and send echo back");
+  }else {
+    res.end("false");
+    console.log("Failed!");
+  }
+}
 
-  //监听文本信息
-  wechat.text(function (data) {
-    //console.log(data.ToUserName);
-    //console.log(data.FromUserName);
-    //console.log(data.CreateTime);
-    //console.log(data.MsgType);
-    //...
-    var msg = {
-      FromUserName : data.ToUserName,
-      ToUserName : data.FromUserName,
-      //MsgType : "text",
-      Content : "这是文本回复",
-      //FuncFlag : 0
-    }
-    //回复信息
-    wechat.send(msg);
-  });
 
-  //监听图片信息
-  //wechat.image(function (data) { ... });
-
-  //监听地址信息
-  //wechat.location(function (data) { ... });
-
-  //监听链接信息
-  //wechat.link(function (data) { ... });
-
-  //监听事件信息
-  //wechat.event(function (data) { ... });
-
-  //监听语音信息
-  //wechat.voice(function (data) { ... });
-
-  //监听视频信息
-  //wechat.video(function (data) { ... });
-
-  //监听所有信息
-  //wechat.all(function (data) { ... });
-}).listen(3000);
+var webSvr = http.createServer(validateToken);
+webSvr.listen(3000,function(){
+  console.log("Start validate");
+});
